4960a8476f44adb8cee647761078ea01c4788d5f
Rakesh Rengaraj <rakesh@ptotem.com>
2016-04-25 13:07:22 +0530

    removed learner name from scorm integration

--- assets/js/scorm-integration.js
+++ assets/js/scorm-integration.js
@@ -7,16 +7,9 @@ function initCourse(min, max){
    //If the scorm.init function succeeded...
    if(lmsConnected){
       //Now let's get the username from the LMS
-      var learnername = scorm.get("cmi.core.student_name");
       scorm.set('cmi.score.min', min);
       scorm.set('cmi.score.max', max);
-      //If the name was successfully retrieved...
-      if(learnername){  
-         //...let's display the username in a page element named "learnername"
-         document.getElementById("learnername").innerHTML = learnername; //use the name in the form
-      }
-   //If the course couldn't connect to the LMS for some reason...
-   } else {
+    } else {
       //... let's alert the user then close the window.
       handleError("Error: Course could not connect with the LMS");
    }
